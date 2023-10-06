c3c967f5a07b81b819f86b75f3080ff1f8ec278f
sjakubenas <sjakubenas@gmail.com>
2023-10-06 07:32:27 -0400

    Relocatte menu related functions to separate file

---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+/** 
+ * Reveals a menu by changind display property
+ * Add a class to menu that gives scaling up effect
+ */
+function openMenu() {
+    // Create menu variable with value of menu element
+    let menu = document.getElementById('menu');
+
+    // Changes display property
+    menu.style.display = 'unset'
+    // Adds a class with timeout method otherwise the effect didnt work
+    setTimeout(function() {
+        menu.classList.add('enlarge-menu')
+    });
+}
+
+/**
+ * Closes the menu by changind the display property 
+ * and removing a class
+ */
+function closeMenu(openMenu) {
+    menu.style.display = 'none';
+    menu.classList.remove('enlarge-menu');
+}
