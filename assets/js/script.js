5bf7d115470b97042d39c5b94f0c4a140e4f0a7e
sjakubenas <sjakubenas@gmail.com>
2023-10-06 07:28:30 -0400

    Relocate button related functions to separate file

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,60 +11,6 @@ let displayReverseCard = document.createElement('img'); // Dealers reverse card
 let countBoxes = document.getElementsByClassName('count-box'); // Assigns a variable to card sum counter boxes
 let outcomeMsg = document.getElementById('outcome-message'); // Displays all outcome messages
 
-// Loads the page before adding event listeners to menu, game buttons and chip selection
-document.addEventListener('DOMContentLoaded', function() {
-    let betButtons = document.getElementsByClassName('bet-chips');
-    let controlButtons = document.getElementsByClassName('control-btn');
-    let menuIcon = document.getElementById('menu-icon');
-    let closeMenuX = document.getElementById('menu-close')
-
-    menuIcon.addEventListener('click', openMenu); //Open menu
-    closeMenuX.addEventListener('click', closeMenu); //Close menu
-    // Iterates betting buttons and determines a bet size which gets passed on to placeBet function
-    for (let betButton of betButtons) {
-        betButton.addEventListener('click', function() {
-            let betSize = this.getAttribute('data-type');
-
-            placeBet(betSize);
-        })
-    }
-    // Iterates control buttons and determines their id and passes that on to gameControls function
-    for (let controlButton of controlButtons) {
-        controlButton.addEventListener('click', function() {
-            let buttonType = this.getAttribute('id');
-
-            gameControls(buttonType);
-        })
-    }
-})
-
-/**
- * Defines control buttons variables
- * And pass it around all game logic 
- */
-function gameControls(buttonType) {
-    let deal = document.getElementById('deal-btn');
-    let hit = document.getElementById('hit-btn');
-    let stand = document.getElementById('stand-btn');
-    let bet = document.getElementById('bet-btn');
-
-    if (buttonType === 'deal-btn') {
-        dealHand(deal, hit, stand, bet);
-    }
-    if (buttonType === 'hit-btn') {
-        playerDraw(hit, stand, bet);
-    }
-    if (buttonType === 'stand-btn') {
-        dealerDraw(hit, stand, bet);
-    }
-    if (buttonType === 'bet-btn') {
-        selectBetSize(deal, bet);
-    }
-    if (buttonType === 'lets-play-btn') {
-        removeGreeting();
-    }
-}
-
 /**
  * Function called by pressing Start Play button
  * Hides the greeting message and overlay
