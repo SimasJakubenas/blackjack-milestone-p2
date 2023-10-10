9cad1476ea453d0ef2d0f82b02fd7f9eebff890c
sjakubenas <sjakubenas@gmail.com>
2023-10-10 13:21:10 -0400

    Update menu open and close functions to account for changes

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,10 +3,6 @@
  * Add a class to menu that gives scaling up effect
  */
 function openMenu() {
-    // looks if menu anchor is focused and changes icon colour if so
-    if (document.querySelector("#menu-anchor:focus")) {
-        document.getElementById('menu-icon').style.color = '#BB0000';
-    }
     // Create menu variable with value of menu element
     const menu = document.getElementById('menu');
 
@@ -22,10 +18,9 @@ function openMenu() {
  * Closes the menu by changind the display property 
  * and removing a class
  */
-function closeMenu(openMenu) {
+function closeMenu() {
     menu.style.display = 'none';
     menu.classList.remove('enlarge-menu');
-    document.getElementById('menu-icon').style.color = '#DDDEC6'; // returns original icon colour
 }
 
 
